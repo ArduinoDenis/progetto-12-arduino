4a22493bc8296286ac0a93bc68d8c6488c9b300e
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-03-03 19:16:02 +0100

    Update modulo_mosfet.ino

--- modulo_mosfet.ino
+++ modulo_mosfet.ino
@@ -1,51 +1,37 @@
 /*
-
-Questo esempio mostra come controllare la velocità di un motore DC
-
+  This example demonstrates how to control the speed of a DC motor using a MOSFET module.
 */
 
-int turns = 0; 
-int turnAmount = 1; 
+int motorSpeed = 0; // Initialize the motor speed variable
+int speedIncrement = 1; // Define the amount by which the speed will be incremented or decremented
 
 unsigned long currentTime;
-
-unsigned long loopTime;
-
- 
+unsigned long previousTime;
 
 void setup() {
-
-   pinMode(9, OUTPUT);
-   currentTime = millis();
-   loopTime = currentTime;
-
+  pinMode(9, OUTPUT); // Set pin 9 as an output for controlling the MOSFET module
+  currentTime = millis(); // Initialize the current time
+  previousTime = currentTime; // Set the previous time equal to the current time
 }
 
-
 void loop() {
+  currentTime = millis(); // Update the current time
 
-   currentTime = millis();
+  // Execute the following code every 20 milliseconds (50 times per second)
+  if (currentTime >= (previousTime + 20)) {
+    analogWrite(9, motorSpeed); // Set the motor speed using PWM on pin 9
+    motorSpeed += speedIncrement; // Increment or decrement the motor speed
 
-   if(currentTime >= (loopTime + 20)){
+    // If the motor speed reaches maximum or minimum, change the direction of speed change
+    if (motorSpeed == 0 || motorSpeed == 255) {
+      speedIncrement = -speedIncrement;
+    }
 
-       analogWrite(9, turns);
+    // If the motor speed reaches 0, pause for 5 seconds
+    if (motorSpeed == 0) {
+      delay(5000);
+    }
 
-       turns = turns + turnAmount;
-
-
-       if (turns == 0 || turns == 255) {
-
-           turnAmount = -turnAmount ;
-
-       }
-
-       if (turns == 0) {
-
-           delay(5000);
-
-       }
-
-       loopTime = currentTime; 
-   }
-
+    previousTime = currentTime; // Update the previous time for the next loop iteration
+  }
 }
